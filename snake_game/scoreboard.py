b852e75a6e3f6b8ec1b8b8146b278efec17d3ce4
Brendan Cardinal <112900230+bc9235@users.noreply.github.com>
2022-11-17 13:23:41 -0800

    Update scoreboard.py
    Add high score

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -11,6 +11,7 @@ class Scoreboard(Turtle):
         """Initialize Scoreboard"""
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.setposition(0, 270)
@@ -18,7 +19,11 @@ class Scoreboard(Turtle):
 
     def display(self):
         """Display scoreboard."""
+        self.clear()
+        self.setposition(-100, 270)
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.setposition(100, 270)
+        self.write(f"High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def add_score(self):
         """Add to score and update scoreboard."""
@@ -29,3 +34,9 @@ class Scoreboard(Turtle):
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER =[", align=ALIGNMENT, font=FONT)
+        
+    def update_high_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.display()
