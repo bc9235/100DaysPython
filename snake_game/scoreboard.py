98908e725475d9f12ec91d38da55c128a1a3d8a8
Brendan Cardinal <112900230+bc9235@users.noreply.github.com>
2022-11-17 13:45:47 -0800

    Update scoreboard.py
    Update high score function, write high score to high_score.txt file

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -37,5 +37,7 @@ class Scoreboard(Turtle):
     def update_high_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", "w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.display()
