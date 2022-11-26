4ee202bf7f9a486029833dbcb7e5a62f6f63913f
Brendan Cardinal <112900230+bc9235@users.noreply.github.com>
2022-11-25 16:43:42 -0800

    Update main.py
    Make json file instead of txt file.  Add search button and search function.  Add error handling.

--- password_manager/main.py
+++ password_manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, shuffle
 import pyperclip
+import json
 
 
 def password_generator():
@@ -29,12 +30,15 @@ def password_generator():
 
 def save_record():
     """Pull data from entry boxes and save to file.  Copy password to clipboard.  Clear entry boxes after saving."""
-    # Gather data and put into entry string
-    delimiter = " | "
+    # Gather data and put into entry
     site = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    entry = f"{site}{delimiter}{username}{delimiter}{password}\n"
+    entry = {
+        site:
+            {"Email/Username": username,
+             "Password": password}
+    }
 
     # Verify all fields have been filled out
     if len(site) == 0 or len(username) == 0 or len(password) == 0:
@@ -44,19 +48,45 @@ def save_record():
         is_ok = messagebox.askyesno(title=site, message=f"Email: {username}\nPassword: {password}\n"
                                                         f"Please confirm you want to add this record.")
         if is_ok:
-            # Write entry to file
-            with open("not_passwords.txt", "a") as record:
-                record.write(entry)
+            try:
+                # Read old data
+                with open("not_passwords.json", "r") as record:
+                    data = json.load(record)
+            except FileNotFoundError:
+                with open("not_passwords.json", "w") as record:
+                    #Write data to new file
+                    json.dump(entry, record, indent=4)
+            else:
+                # Update old data with new data
+                data.update(entry)
+                with open("not_passwords.json", "w") as record:
+                    # Write updated data
+                    json.dump(data, record, indent=4)
+                # Copy password to clipboard
+                pyperclip.copy(password)
+            finally:
+                # Delete data from site and password entries
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
-            # Copy password to clipboard
-            pyperclip.copy(password)
-
-            # Delete data from site and password entries
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
-
-            # Reset cursor to website entry
-            website_entry.focus()
+                # Reset cursor to website entry
+                website_entry.focus()
+                
+                
+def search_records():
+    """Search records for website entry."""
+    website = website_entry.get()
+    try:
+        with open("not_passwords.json", "r") as record:
+            entries = json.load(record)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Error", message="Data file not found.\nCreate record to create file.")
+    else:
+        if website in entries:
+            messagebox.showinfo(title=website, message=f"Email/Username: {entries[website]['Email/Username']}"
+                                                       f"\nPassword: {entries[website]['Password']}")
+        else:
+            messagebox.showwarning(title="Error", message=f"{website} record not found.")
 
 
 # Window Setup
@@ -73,16 +103,20 @@ canvas.grid(column=1, row=0)
 # Website Label and Entry
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-website_entry = Entry(width=47)
+website_entry = Entry(width=28)
 website_entry.focus()  # Start with cursor in website_box
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
+
+# Search Button
+search_button = Button(text="Search", command=search_records, width=15)
+search_button.grid(column=2, row=1)
 
 # Email/Username Label and Entry
 username_label = Label(text="Email/Username:")
 username_label.grid(column=0, row=2)
-username_entry = Entry(width=47)
+username_entry = Entry(width=28)
 username_entry.insert(0, "username@email.com")  # Autofill email address
-username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.grid(column=1, row=2)
 
 # Password Label, Entry, Generate Password Button
 password_label = Label(text="Password:")
