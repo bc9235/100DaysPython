b0142f98db30f285e88500575b11679d9e0d06f5
Brendan Cardinal <112900230+bc9235@users.noreply.github.com>
2022-12-05 13:17:39 -0800

    Update main.py

--- flight_deals/main.py
+++ flight_deals/main.py
@@ -6,8 +6,16 @@ sheet_data = DataManager()
 flight_search = FlightSearch()
 notifications = NotificationManager()
 
+while True:
+    # Create user
+    create = sheet_data.create_user()
+    if create:
+        break
+
 # Get current location IATA code
 here = flight_search.current_location_iata()
+
+print("Please wait while we search...")
 
 # Get destination from Google Sheet
 destinations = sheet_data.get_destinations()
@@ -24,9 +32,14 @@ price_data = flight_search.pull_flight_data(iata_codes=codes, current_location=h
 # Pull low prices from Google sheet
 low_prices = sheet_data.get_low_prices()
 
+# Pull email addresses from Google sheet
+email_list = sheet_data.get_email_list()
+
 # Compare current price to low price
 for key in low_prices:
     # If current price lower than low price, send alert message
     if low_prices[key] < price_data[key]:
         message = f"Low price alert!\n{here} -> {key}: ${price_data[key]}"
         notifications.send_message(message_content=message)
+        notifications.send_emails(email_list=email_list, message_content=message)
+print("Success!  Notifications have been sent.")
