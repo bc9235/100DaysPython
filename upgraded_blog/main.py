08a5a5a0d50866183c08dd2011f6bfc95595281b
Brendan Cardinal <112900230+bc9235@users.noreply.github.com>
2022-12-17 14:47:37 -0800

    Update main.py
    Make contact page work, gather data that was submitted, and send email.

--- upgraded_blog/main.py
+++ upgraded_blog/main.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL = os.environ["EMAIL"]
+PASS = os.environ["PASS"]
+PROVIDER = os.environ["PROVIDER"]
+PORT = int(os.environ["PORT"])
 
 app = Flask(__name__)
 
@@ -11,14 +21,19 @@ def home():
     return render_template("index.html", content=posts)
 
 
-@app.route("/about.html")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact.html")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    else:
+        return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/post/<int:index>")
@@ -28,6 +43,14 @@ def post(index):
         if entry["id"] == index:
             requested_post = entry
     return render_template("post.html", content=requested_post)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP(PROVIDER, PORT) as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASS)
+        connection.sendmail(EMAIL, EMAIL, email_message)
 
 
 if __name__ == "__main__":
